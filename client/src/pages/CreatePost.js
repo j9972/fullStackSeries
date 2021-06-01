75b41e09d1b420e8f6d85fda0c30795deffe68ad
jung su young <j9972@naver.com>
2021-06-01 23:05:28 +0900

    install react-router-dom and using it

---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import axios from "axios";
+// import {useEffect, useState} from "react";
+
+function CreatePost() {
+    return (
+        <div>
+            create
+        </div>
+    )
+}
+
+export default CreatePost
