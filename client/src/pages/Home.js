403d7b5726d779bd79cf2c2b946ea69a4d9d899d
jung su young <j9972@naver.com>
2021-06-02 22:31:13 +0900

    using useHistory

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 function Home() {
+  const [listOfPosts, setListOfPosts] = useState([]);
+  let history = useHistory();
 
-    const [listOfPosts, setListOfPosts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setListOfPosts(response.data);
+    });
+  }, []);
 
-    useEffect(() => {
-      axios.get("http://localhost:3001/posts").then((res) => {
-        setListOfPosts(res.data);
-      })
-    }, []);
-
-    return (
-        <div>
-            {listOfPosts.map((value, key) => {
-                return (  
-                <div className="post">
-                    <div className="title">
-                    {value.title}
-                    </div>
-                    <div className="body">
-                    {value.postText}
-                    </div>
-                    <div className="footer">
-                    {value.username}
-                    </div>
-                </div>
-                );
-            })}
-        </div>
-    )
+  return (
+    <div>
+      {listOfPosts.map((value, key) => {
+        return (
+          <div
+            className="post"
+            onClick={() => {
+              history.push(`/post/${value.id}`);
+            }}
+          >
+            <div className="title"> {value.title} </div>
+            <div className="body">{value.postText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
