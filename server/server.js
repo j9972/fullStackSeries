400ef6460916c5678fb9124603ab12e24d2dfcce
jung su young <j9972@naver.com>
2021-06-06 23:10:02 +0900

    copying code

--- server/server.js
+++ server/server.js
@@ -1,25 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 
-// sequelize를 위해서 connecting with model folder 
-const db = require('./models');
+const db = require("./models");
 
 // Routers
-const postRouter = require('./routes/Posts');
-app.use('/posts', postRouter);
-const commentsRouter = require('./routes/Comments');
-app.use('/comments', commentsRouter);
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
+const usersRouter = require("./routes/Users");
+app.use("/auth", usersRouter);
+const likesRouter = require("./routes/Likes");
+app.use("/likes", likesRouter);
 
-
-
-// make a table by using sequelize
 db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log("Server is running on port 3001");
-    });
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
 });
-
